d617c6131aa91b3950dfc57fd9507c5d60135a4b
shanmu20140907 <70598816+shanmu20140907@users.noreply.github.com>
2022-02-11 23:32:23 +0000

    Update EmailAddress.sql

--- Person/Tables/EmailAddress.sql
+++ Person/Tables/EmailAddress.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Person].[EmailAddress] (
     [BusinessEntityID] INT              NOT NULL,
     [EmailAddressID]   INT              IDENTITY (1, 1) NOT NULL,
-    [EmailAddress]     NVARCHAR (50)    NULL,
+    [EmailAddress]     NVARCHAR (250)    NULL,
     [rowguid]          UNIQUEIDENTIFIER CONSTRAINT [DF_EmailAddress_rowguid] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
     [ModifiedDate]     DATETIME         CONSTRAINT [DF_EmailAddress_ModifiedDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_EmailAddress_BusinessEntityID_EmailAddressID] PRIMARY KEY CLUSTERED ([BusinessEntityID] ASC, [EmailAddressID] ASC),
